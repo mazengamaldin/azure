e2fa03e478f4cc8a0b6090f3e83620af6155a2af
mazengamaldin <mazen.gamaldin@gmail.com>
2025-10-09 23:17:26 +0300

    7th attempt with subnet

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-resource "azurerm_resource_group" "rg" {
+/*resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
-
+*/
 resource "azurerm_virtual_network" "mazen_vnet" {
     resource_group_name = var.resource_group_name
     name = var.vnet_name
