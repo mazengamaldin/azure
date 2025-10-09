6c1f5f4e2e30cd53d20bc9b2f1737c45f577667b
mazengamaldin <mazen.gamaldin@gmail.com>
2025-10-09 23:05:12 +0300

    7th attempt with subnet

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "github"
+    storage_account_name = "mazentfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 
