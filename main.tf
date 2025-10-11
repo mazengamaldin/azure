1636640f3789a37e5816246dc65a0c75bd14858e
mazengamaldin <mazen.gamaldin@gmail.com>
2025-10-11 23:49:06 +0300

    auto deploy image to container app attempt7

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg1" {
   name     = var.resource_group_name
   location = var.location
 }
